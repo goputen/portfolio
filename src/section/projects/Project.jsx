acbaaa2a18f6371bb6ae79a4704e8c04009cbc54
Sachin <146167233+goputen@users.noreply.github.com>
2024-07-25 11:58:07 +0530

    Update Project.jsx

--- src/section/projects/Project.jsx
+++ src/section/projects/Project.jsx
@@ -2,6 +2,7 @@ import ProjectCard from './card/ProjectCard'
 import viberr from '../../assets/mitlogo.png'
 import styles from './Project.module.css'
 import moviescreenshot from '../../assets/moviescreenshot.png'
+import projectPic from '../../assets/project.PNG'
 function Project() {
   return (
 <section className={styles.container}>
@@ -9,7 +10,7 @@ function Project() {
   <div className={styles.projectContainer}>
    <ProjectCard viberr={viberr} h3="Paper Website" p="full-stack project" link="http://mitpaperdown.rf.gd/MIT/login.php/"></ProjectCard>
    <ProjectCard viberr={moviescreenshot} h3="Movie website" p="front-end project" link="https://goputen.github.io/movieszone/"></ProjectCard>
-    <ProjectCard viberr={moviescreenshot} h3="IMDB clone" p="front-end project" link="https://imdb-clone-one-rho.vercel.app/"></ProjectCard>
+    <ProjectCard viberr={projectPic} h3="IMDB clone" p="front-end project" link="https://imdb-clone-one-rho.vercel.app/"></ProjectCard>
   </div>
 </section>
   )
